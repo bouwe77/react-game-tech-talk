22230b97367ee3632edefbd36bc33a48ec43a70a
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-05-06 22:17:42 +0200

    .

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -12,7 +12,7 @@ const getMaze = createGetMaze()
 const updateMaze = createUpdateMaze()
 
 const initialState = {
-  getMaze: () => getMaze(1),
+  getMaze: () => getMaze(2),
   direction: directions.NONE,
   interval: 400,
 }
